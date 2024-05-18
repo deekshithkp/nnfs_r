418bcf95335a409ba6c10c1bbf903cb86239577c
deekkay <10346316+deekshithkp@users.noreply.github.com>
2024-05-18 12:25:16 +0530

    - Layer of neurons concepts - Changed zip logic for dot product calculation

--- nnfs_r/src/main.rs
+++ nnfs_r/src/main.rs
@@ -1,3 +1,5 @@
+use std::iter::zip;
+
 
 fn main() {
     // Single Neuron concepts with 3 inputs (each with a bias)
@@ -15,5 +17,30 @@ fn main() {
 
     let output = dot_product + bias;
 
-    print!("Neuron output: {output}");
+    println!("Neuron output: {output}");
+
+    // Layer of Neurons concepts with 3 neurons connected to the same input but with different weights and biases
+    let inputs = [1.0, 2.0, 3.0, 2.5];
+    let weights = [
+            [0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87],
+        ];
+    let biases = [2.0, 3.0, 0.5];
+
+    let mut layer_outputs: Vec<f64> = Vec::new();
+
+    for (neuron_weights, neuron_bias) in zip(weights, biases){
+        let mut neuron_output = 0.0;
+
+        for (n_input, weight) in zip(inputs, neuron_weights) {
+            neuron_output += n_input * weight;
+        }
+
+        neuron_output += neuron_bias;
+
+        layer_outputs.push(neuron_output);
+    }
+
+    println!("Neuron Layer output: {:?}", layer_outputs);
 }
