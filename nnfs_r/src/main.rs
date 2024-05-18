5919413e6970fbc58a226dffa2f42f8e6f273500
deekkay <10346316+deekshithkp@users.noreply.github.com>
2024-05-18 12:52:53 +0530

    - Modified single neuron implementation with ndarray

--- nnfs_r/src/main.rs
+++ nnfs_r/src/main.rs
@@ -1,22 +1,17 @@
 use std::iter::zip;
+use ndarray::arr1;
 
 
 fn main() {
     // Single Neuron concepts with 3 inputs (each with a bias)
     // Each neuron holds a weight
-    // Concepts for introduction purposes; would be scratched off later
+    // Implementation changed to use ndarray - dot product
 
-    let inputs = [1.0, 2.0, 3.0, 2.5];
-    let weights = [0.2, 0.8, -0.5, 1.0];
+    let inputs = arr1(&[1.0, 2.0, 3.0, 2.5]);
+    let weights = arr1(&[0.2, 0.8, -0.5, 1.0]);
     let bias = 2.0;
 
-    let dot_product: f64 = inputs.iter()
-        .zip(weights.iter())
-        .map(|(input, weight)| input * weight)
-        .sum();
-
-    let output = dot_product + bias;
-
+    let output: f64 = inputs.dot(&weights) + bias;
     println!("Neuron output: {output}");
 
     // Layer of Neurons concepts with 3 neurons connected to the same input but with different weights and biases
