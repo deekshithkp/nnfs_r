cf547346cb362e2321983ba2a33fae3fdf510225
deekkay <10346316+deekshithkp@users.noreply.github.com>
2024-05-22 07:56:25 +0530

    SoftMax Activation function implementation

--- nnfs_r/src/main.rs
+++ nnfs_r/src/main.rs
@@ -1,5 +1,6 @@
-use ndarray::Array2;
+use ndarray::{Axis,Array2};
 use ndarray_rand::{self, rand_distr::Uniform, RandomExt};
+use ndarray_stats::QuantileExt;
 
 
 #[derive(Debug)]
@@ -40,18 +41,58 @@ impl ActivationReLU {
     }
 }
 
+struct ActivationSoftMax {
+    outputs: Array2<f64>,
+}
+
+impl ActivationSoftMax {
+    pub fn new() -> Self {
+        ActivationSoftMax {
+            outputs: Array2::zeros((0, 0)),
+        }
+    }
+
+    // SoftMax is an exponential function that helps us classify the input into probabilities ideal for output layer
+    // The input is exponentiated (only positive values), normalised (minimise variance) and grouped to classifications
+    pub fn forward(&mut self, inputs: &Array2<f64>) {
+        // The exp value can skyrocket for large positive values, hence we calculate the max value for each row and subtract it from each value
+        let max_values = inputs.map_axis(Axis(1), |row| *row.max().unwrap());
+        
+        // Subtract the max values from each row and compute the unnormalised exponentials
+        // Notice that the max 1D array max_values is inserted with Axis 1 for compatibility
+        let exp_values = inputs - &max_values.insert_axis(Axis(1));
+        let exp_values = exp_values.mapv(f64::exp);
+
+        // Compute the sum of exponentials along axis 1
+        let sum_exp_values = exp_values.sum_axis(Axis(1));
+
+        // Normalize probabilities for each sample
+        let probabilities = exp_values / &sum_exp_values.insert_axis(Axis(1));
+
+        // Store the result in the output
+        self.outputs = probabilities;
+
+    }
+}
+
 fn main() {
     // Notice that the shape for the inputs and Neuron's weights match
     // Random data is fine for now but we would need to have the sample data in a persistent storage (likely a file) when we get to training the network
     let inputs = Array2::random((6, 4), Uniform::new(-2.5, 2.53));
-    let mut dense_layer = DenseLayer::new(4, 3);
+    let mut dense_layer1 = DenseLayer::new(4, 3);
 
-    println!("Layer: {:?}", dense_layer);
-    dense_layer.forward(inputs);
-
-    println!("Output: {:?}", dense_layer.outputs);
+    dense_layer1.forward(inputs);
+    println!("Layer 1 Output: {:?}", dense_layer1.outputs);
 
     let mut activation_relu = ActivationReLU::new();
-    activation_relu.forward(&dense_layer.outputs);
+    activation_relu.forward(&dense_layer1.outputs);
     println!("ReLU Output: {:?}", activation_relu.outputs);
+
+    let mut dense_layer2 = DenseLayer::new(3, 3);
+    dense_layer2.forward(activation_relu.outputs);
+    println!("Layer 2 Output: {:?}", dense_layer2.outputs);
+
+    let mut activation_softmax = ActivationSoftMax::new();
+    activation_softmax.forward(&dense_layer2.outputs);
+    println!("SoftMax Output: {:?}", activation_softmax.outputs);
 }
